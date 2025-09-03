76a473dc8c58c29cb21fc1e288e04502cb2d869f
Sravan Surya <vsravansurya@gmail.com>
2025-09-03 16:39:48 +0530

    Creating a Secure Signup Route

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express"
+import {userRoute} from "./routes/user.route.js"
 
 const app = express()
 const PORT = process.env.PORT ?? 8000
+
+app.use(express.json())
+
+app.use("/user", userRoute)
 
 app.get("/",(req,res)=>{
     res.status(200).json({message:`server is running at port ${PORT}`})
